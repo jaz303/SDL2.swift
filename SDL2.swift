70c5f686d7377f8f735d664de1a92a6d5d2f9026
Jason Frame <jason@onehackoranother.com>
2016-02-23 23:16:12 +0000

    surface fill rect(s), surface clippping

--- SDL2.swift
+++ SDL2.swift
@@ -632,9 +632,6 @@ public class Event {
 
 // TODO: SDL_ConvertSurface
 // TODO: SDL_CreateRGBSurfaceFrom
-// TODO: SDL_FillRect
-// TODO: SDL_FillRects
-// TODO: SDL_(Get|Set)ClipRect
 // TODO: SDL_(Get|Set)ColorKey
 // TODO: SDL_(Get|Set)SurfaceAlphaMod
 // TODO: SDL_(Get|Set)SurfaceBlendMode
@@ -695,6 +692,7 @@ public class Surface {
 	 * unaltered, i.e. no color format conversion is performed.
 	 *
 	 * FIXME: this assumes 4 bytes per pixel
+	 * FIXME: endianness issues?
 	 */
 	public func putPixel32(x: Int, _ y: Int, _ color: Uint32) {
 		let px = UnsafeMutablePointer<UInt8>(COpaquePointer(theSurface.memory.pixels))
@@ -712,6 +710,32 @@ public class Surface {
 
 	public func unlock() {
 		SDL_UnlockSurface(theSurface)
+	}
+
+	public func fillRect(inout rect: Rect, color: UInt32) {
+		SDL_FillRect(theSurface, &rect, color)
+	}
+
+	public func fillRects(rects: [Rect], color: UInt32) {
+		SDL_FillRects(theSurface, rects, Int32(rects.count), color)
+	}
+
+	public func clipRect() -> Rect {
+		var r = Rect()
+		SDL_GetClipRect(theSurface, &r)
+		return r
+	}
+
+	public func clipRect(inout rect: Rect) {
+		SDL_GetClipRect(theSurface, &rect)
+	}
+
+	public func clearClipRect() {
+		SDL_SetClipRect(theSurface, nil)
+	}
+
+	public func setClipRect(inout rect: Rect) {
+		SDL_SetClipRect(theSurface, &rect)
 	}
 
 	public func blitSurface(source: Surface, inout srcRect : Rect, inout destRect: Rect) {
