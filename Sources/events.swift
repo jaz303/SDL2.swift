858f5f728875956b926b03ee98eee51072c71a59
Jason Frame <jason@onehackoranother.com>
2016-02-29 08:00:55 +0000

    Event is just an extension on SDL_Event now

--- Sources/events.swift
+++ Sources/events.swift
@@ -20,7 +20,7 @@ public class Events {
 	}
 
 	public class func push(inout evt: Event) {
-		SDL_PushEvent(&evt.raw)
+		SDL_PushEvent(&evt)
 	}
 
 	public class func poll() -> Event? {
@@ -33,7 +33,7 @@ public class Events {
 	}
 
 	public class func poll(inout evt: Event) -> Bool {
-		return SDL_PollEvent(&evt.raw) == Int32(1)
+		return SDL_PollEvent(&evt) == Int32(1)
 	}
 
 	public class func wait() -> Event {
@@ -52,11 +52,11 @@ public class Events {
 	}
 
 	public class func wait(inout evt: Event) {
-		SDL_WaitEvent(&evt.raw)
+		SDL_WaitEvent(&evt)
 	}
 
 	public class func wait(inout evt: Event, timeout: Int) -> Bool {
-		return SDL_WaitEventTimeout(&evt.raw, Int32(timeout)) == 1
+		return SDL_WaitEventTimeout(&evt, Int32(timeout)) == 1
 	}
 
 	public class func flush(type: Uint32) {
@@ -84,116 +84,108 @@ public class Events {
 	}
 }
 
-public class Event {
-	public init() {
-		raw = SDL_Event()
-	}
-
-	public var type: Uint32 { get { return raw.type } }
-
+public extension Event {
 	// NOTE: this should be safe, structs are all laid out identically
-	public var timestamp: Uint32 { get { return raw.motion.timestamp } }
-	public var windowID: Uint32 { get { return raw.motion.windowID } }
+	public var timestamp: Uint32 { get { return self.motion.timestamp } }
+	public var windowID: Uint32 { get { return self.motion.windowID } }
 
 	//
 	// App
 
 	public var isQuit: Bool {
-		get { return raw.type == Uint32(K_SDL_QUIT) }
+		get { return self.type == Uint32(K_SDL_QUIT) }
 	}
 
 	//
 	// Window
 
 	public var isWindow: Bool {
-		get { return raw.type == Uint32(K_SDL_WINDOWEVENT) }
+		get { return self.type == Uint32(K_SDL_WINDOWEVENT) }
 	}
 
 	public var isWindowClose: Bool {
-		get { return raw.window.event == Uint8(K_SDL_WINDOWEVENT_CLOSE) }
+		get { return self.window.event == Uint8(K_SDL_WINDOWEVENT_CLOSE) }
 	}
 
 	// Keyboard
 
 	public var isKeyDown: Bool {
-		get { return raw.type == Uint32(K_SDL_KEYDOWN) }
+		get { return self.type == Uint32(K_SDL_KEYDOWN) }
 	}
 
 	public var isKeyUp: Bool {
-		get { return raw.type == Uint32(K_SDL_KEYUP) }
+		get { return self.type == Uint32(K_SDL_KEYUP) }
 	}
 
 	public var isTextEditing: Bool {
-		get { return raw.type == Uint32(K_SDL_TEXTEDITING) }
+		get { return self.type == Uint32(K_SDL_TEXTEDITING) }
 	}
 
 	public var isTextInput: Bool {
-		get { return raw.type == Uint32(K_SDL_TEXTINPUT) }
+		get { return self.type == Uint32(K_SDL_TEXTINPUT) }
 	}
 
 	//public var isKeyMapChanged: Bool {
-	//	get { return raw.type == Uint32(K_SDL_KEYMAPCHANGED) }
+	//	get { return self.type == Uint32(K_SDL_KEYMAPCHANGED) }
 	//}
 
 	// Mouse
 	// TODO: "state" member
 
 	public var isMouseMotion: Bool {
-		get { return raw.type == Uint32(K_SDL_MOUSEMOTION) }
+		get { return self.type == Uint32(K_SDL_MOUSEMOTION) }
 	}
 
-	public var mouseMotionWhich: Uint32 { get { return raw.motion.which } }
-	public var mouseMotionX: Int { get { return Int(raw.motion.x) } }
-	public var mouseMotionY: Int { get { return Int(raw.motion.y) } }
-	public var mouseMotionDX: Int { get { return Int(raw.motion.xrel) } }
-	public var mouseMotionDY: Int { get { return Int(raw.motion.yrel) } }
+	public var mouseMotionWhich: Uint32 { get { return self.motion.which } }
+	public var mouseMotionX: Int { get { return Int(self.motion.x) } }
+	public var mouseMotionY: Int { get { return Int(self.motion.y) } }
+	public var mouseMotionDX: Int { get { return Int(self.motion.xrel) } }
+	public var mouseMotionDY: Int { get { return Int(self.motion.yrel) } }
 	
 	public var isMouseButtonDown: Bool {
-		get { return raw.type == Uint32(K_SDL_MOUSEBUTTONDOWN) }
+		get { return self.type == Uint32(K_SDL_MOUSEBUTTONDOWN) }
 	}
 
 	public var isMouseButtonUp: Bool {
-		get { return raw.type == Uint32(K_SDL_MOUSEBUTTONUP) }
+		get { return self.type == Uint32(K_SDL_MOUSEBUTTONUP) }
 	}
 
 	public var isLeftMouseButton: Bool {
-		get { return raw.button.button == Uint8(SDL_BUTTON_LEFT) }
+		get { return self.button.button == Uint8(SDL_BUTTON_LEFT) }
 	}
 
 	public var isMiddleMouseButton: Bool {
-		get { return raw.button.button == Uint8(SDL_BUTTON_MIDDLE) }
+		get { return self.button.button == Uint8(SDL_BUTTON_MIDDLE) }
 	}
 
 	public var isRightMouseButton: Bool {
-		get { return raw.button.button == Uint8(SDL_BUTTON_RIGHT) }
+		get { return self.button.button == Uint8(SDL_BUTTON_RIGHT) }
 	}
 
 	public var isX1MouseButton: Bool {
-		get { return raw.button.button == Uint8(SDL_BUTTON_X1) }
+		get { return self.button.button == Uint8(SDL_BUTTON_X1) }
 	}
 
 	public var isX2MouseButton: Bool {
-		get { return raw.button.button == Uint8(SDL_BUTTON_X2) }
+		get { return self.button.button == Uint8(SDL_BUTTON_X2) }
 	}
 
-	public var mouseButtonWhich: Uint32 { get { return raw.button.which } }
+	public var mouseButtonWhich: Uint32 { get { return self.button.which } }
 
 	// TODO: replace this with an internal constant
-	public var mouseButtonButton: Int { get { return Int(raw.button.button) } }
-	public var mouseButtonX: Int { get { return Int(raw.button.x) } }
-	public var mouseButtonY: Int { get { return Int(raw.button.y) } }
-	public var mouseButtonClicks: Int { get { return Int(raw.button.clicks) } }
+	public var mouseButtonButton: Int { get { return Int(self.button.button) } }
+	public var mouseButtonX: Int { get { return Int(self.button.x) } }
+	public var mouseButtonY: Int { get { return Int(self.button.y) } }
+	public var mouseButtonClicks: Int { get { return Int(self.button.clicks) } }
 
 	public var isMouseWheel: Bool {
-		get { return raw.type == Uint32(K_SDL_MOUSEWHEEL) }
+		get { return self.type == Uint32(K_SDL_MOUSEWHEEL) }
 	}
 
-	public var mouseWheelWhich: Uint32 { get { return raw.wheel.which } }
-	public var mouseWheelDX: Int { get { return Int(raw.wheel.x) } }
-	public var mouseWheelDY: Int { get { return Int(raw.wheel.y) } }
+	public var mouseWheelWhich: Uint32 { get { return self.wheel.which } }
+	public var mouseWheelDX: Int { get { return Int(self.wheel.x) } }
+	public var mouseWheelDY: Int { get { return Int(self.wheel.y) } }
 	
 	// TODO: "direction" member (constantify)
-	//public var mouseWheelIsFlipped: Bool { get { return raw.wheel.direction == SDL_MOUSEWHEEL_FLIPPED } }
-
-	public var raw: SDL_Event;
+	//public var mouseWheelIsFlipped: Bool { get { return self.wheel.direction == SDL_MOUSEWHEEL_FLIPPED } }
 }
--- Sources/types.swift
+++ Sources/types.swift
@@ -2,3 +2,4 @@ import CSDL2
 
 public typealias TimerCallback = (Int) -> (Int)
 public typealias TimerID = SDL_TimerID
+public typealias Event = SDL_Event
