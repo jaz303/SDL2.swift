368f3e1d0c746cad864a3c4eadfc5504a55981ff
Jason Frame <jason@onehackoranother.com>
2016-12-23 19:33:55 +0000

    timers

--- Sources/timers.swift
+++ Sources/timers.swift
@@ -1,25 +1,26 @@
 import CSDL2
 
 extension sdl {
-	public class func delay(delay: Int) {
+	public class func delay(ms delay: Int) {
 		SDL_Delay(UInt32(delay))
 	}
 
-	// FIXME
-	// public class func setTimeout(delay: Int, callback: TimerCallback) -> TimerID {
-	// 	let box = Box(callback)
-	// 	let opq = Unmanaged.passRetained(box).toOpaque()
-	// 	let mut = UnsafeMutableRawPointer(opq)
-	// 	return SDL_AddTimer(UInt32(delay), { (delay,userdata) in
-	// 		let opq = OpaquePointer(userdata)
-	// 		let box: Box<TimerCallback> = Unmanaged
-	// 										.fromOpaque(opq)!
-	// 										.takeRetainedValue()
-	// 		return UInt32(box.value(Int(delay)))
-	// 	}, mut)
-	// }
+	public class func setTimeout(delay: Int, callback: TimerCallback) -> TimerID {
+		let opq = Unmanaged.passRetained(Box(callback)).toOpaque()
+		let mut = UnsafeMutableRawPointer(opq)
+		return SDL_AddTimer(UInt32(delay), { (delay,userdata) in
+			let opq = UnsafeRawPointer(userdata)!
+			let box: Box<TimerCallback> = Unmanaged
+											.fromOpaque(opq)
+											.takeRetainedValue()
+			return UInt32(box.value(Int(delay)))
+		}, mut)
+	}
 
-	public class func clearTimeout(timerId: TimerID) -> Bool {
+	// FIXME: think this might cause a memory leak as the callback will never
+	// be released. Might need to maintain an internal map of timer IDs and
+	// manage them manually...
+	public class func clearTimeout(id timerId: TimerID) -> Bool {
 		return SDL_RemoveTimer(timerId) == SDL_TRUE
 	}
 
