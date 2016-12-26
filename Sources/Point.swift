74685b7b98aea9fc3230866f4b9bd746dc6e6c15
Jason Frame <jason@onehackoranother.com>
2016-12-26 12:11:47 +0000

    convenience functions for Point

--- Sources/Point.swift
+++ Sources/Point.swift
@@ -2,7 +2,17 @@ import CSDL2
 
 public typealias Point = SDL_Point
 extension Point {
-	
+	public func isZero() -> Bool {
+		return x == 0 && y == 0
+	}
+
+	public func inverted() -> Point {
+		return Point(x: -x, y: -y)
+	}
+
+	public mutating func invert() {
+		x = -x; y = -y
+	}
 }
 
 public func +(left: Point, right: Point) -> Point {
